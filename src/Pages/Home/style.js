61e76ed7744aa600569af792839b12b7f89d61e3
Marco Arruda <marcoantonioarrudadasilva@gmail.com>
2022-06-09 11:50:00 -0300

    fix: arrumando o scroll da tela

--- src/Pages/Home/style.js
+++ src/Pages/Home/style.js
@@ -6,7 +6,7 @@ export const Conteiner = styled.div`
 `;
 
 export const Content = styled.div`
-    width: 100%;
+    /* width: 100%; */
     display: flex;
     justify-content: center;
 `;
--- src/components/Showcase/style.js
+++ src/components/Showcase/style.js
@@ -1,21 +1,30 @@
 import styled from "styled-components";
 
 export const Conteiner = styled.main`
-  width: 100%;
   display: flex;
   justify-content: center;
 `;
 
-export const Content = styled.article`
-  width: 100%;
+export const Content = styled.section`
   display: flex;
   justify-content: center;
 
   ul {
-    width: 80%;
+    width: 100vw;
+    overflow-x: scroll;
 
     display: flex;
-    justify-content: center;
+
     align-items: center;
   }
+
+  @media (min-width: 768px) {
+  
+  ul {
+    overflow-x: hidden;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+
 `;
