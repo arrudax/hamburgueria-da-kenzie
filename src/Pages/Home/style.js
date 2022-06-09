b65833b7ca482e0412e466d340595b7e080b6c84
Marco Arruda <marcoantonioarrudadasilva@gmail.com>
2022-06-09 11:56:24 -0300

    fix: ajustando diferenças do merge

--- src/Pages/Home/style.js
+++ src/Pages/Home/style.js
@@ -1,19 +1,10 @@
 import styled from "styled-components";
 
-export const Conteiner = styled.div`
-  
- 
-`;
+export const Conteiner = styled.div``;
 
-<<<<<<< HEAD
-export const Content = styled.main`
-    width: 100%;
-=======
 export const Content = styled.div`
-    /* width: 100%; */
->>>>>>> feature/card
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 
-    overflow: hidden;
+  overflow: hidden;
 `;
--- src/components/Showcase/style.js
+++ src/components/Showcase/style.js
@@ -1,25 +1,11 @@
 import styled from "styled-components";
 
-<<<<<<< HEAD
-export const Conteiner = styled.article`
-  width: 100%;
-=======
 export const Conteiner = styled.main`
->>>>>>> feature/card
   display: flex;
   justify-content: center;
 `;
 
 export const Content = styled.section`
-<<<<<<< HEAD
-  width: 100%;
-  display: flex;
-  justify-content: center;
-
-  ul {  
-    overflow-x: auto;
-    
-=======
   display: flex;
   justify-content: center;
 
@@ -27,19 +13,16 @@ export const Content = styled.section`
     width: 100vw;
     overflow-x: scroll;
 
->>>>>>> feature/card
     display: flex;
 
     align-items: center;
   }
 
   @media (min-width: 768px) {
-  
-  ul {
-    overflow-x: hidden;
-    flex-wrap: wrap;
-    justify-content: center;
+    ul {
+      overflow-x: hidden;
+      flex-wrap: wrap;
+      justify-content: center;
+    }
   }
-}
-
 `;
